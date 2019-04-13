f15b48bfa0b8801f10325bb172ccc07729997095
EvyHaan <evy.haan@gmail.com>
2019-04-13 14:17:45 -0700

    finished views tests for now

--- budget_tool_app/test_views.py
+++ budget_tool_app/test_views.py
@@ -29,3 +29,35 @@ class TestBudgetViews(TestCase):
         self.assertIn(own_budget.name.encode(), res.content)
         self.assertNotIn(other_budget.name.encode(), res.content)
 
+
+class TestBudgetCreateView(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.user.set_password('password')
+        self.user.save()
+        self.c = Client()
+
+    def test_create_view_adds_new_budget(self):
+        self.c.login(username=self.user.username, password='password')
+        form_data = {'name': 'Budget 1', 'budget_total': 42}
+        res = self.c.post('/budget/add', form_data, follow=True)
+        self.assertIn(b'Budget 1', res.content)
+    
+
+class TestCardCreateView(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.user.set_password('password')
+        self.user.save()
+        self.c = Client()
+        self.budget = BudgetFactory(user=self.user)
+    
+
+    #need help understanding this test... it is not passing, I think because it is not hitting the url which requires a pk for budget id.
+    # def test_create_view_adds_new_transaction(self):
+    #     self.c.login(username=self.user.username, password='password')
+    #     form_data = {'type_of': 'withdrawal', 'amount': 42, 'description': 'something descriptive', 'budget': self.budget.id}
+    #     res = self.c.post('{}/transaction/add'.format(self.budget.id), form_data, follow=True)
+
+    #     self.assertIn(b'withdrawal', res.content)
+
